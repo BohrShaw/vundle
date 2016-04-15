9793c16f56561b3d5759aa59f203ecfed5baf0a5
Bohr Shaw <bohrshaw@gmail.com>
2016-04-15 13:53:23 +0800

    Improve TestBundleDecode()

--- vundle_test.go
+++ vundle_test.go
@@ -7,10 +7,12 @@ import (
 
 func TestBundleDecode(t *testing.T) {
 	bs := []string{"foo/bar", "foo/bar/baz", "a/b:", "a/b:br", "a/b:br/dir/dir"}
-	f := regexp.MustCompile(`^[[:word:]-.]+/[[:word:]-.:]+$`)
+	fr := regexp.MustCompile(`^[[:word:]-.]+/[[:word:]-.]+$`)
+	fb := regexp.MustCompile(`^[[:word:]-.]*$`)
 	for _, b := range bs {
-		if !f.MatchString(bundleDecode(b).repo) {
-			t.Fatal("Bundle format %v is mis-decoded.", b)
+		bd := bundleDecode(b)
+		if !fr.MatchString(bd.repo) || !fb.MatchString(bd.branch) {
+			t.Errorf("Bundle format %v is mis-decoded as %v.", b, bd)
 		}
 	}
 }
